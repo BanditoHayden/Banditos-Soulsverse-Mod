1b8990f0bca0907979e23404b8868fb334dcc5e1
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-04-18 17:14:05 -0700

    Add files via upload

---
+++ Buffs/GoldenVow/GoldenVow.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace SoulsMod.Buffs.GoldenVow
+{
+    public class GoldenVow : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Golden Vow");
+            Description.SetDefault("Grants +10 defense\n+20% Damage");
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.statDefense += 10;
+            player.GetDamage(DamageClass.Generic) += 0.2f;
+
+        }
+    }
+}
---
+++ Buffs/WeaponArtCooldown.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Threading.Tasks;
+
+namespace SoulsMod.Buffs
+{
+    public class WeaponArtCooldown : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Weapon Art Cooldown");
+            Description.SetDefault("Prevents the player from using a weapon art");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+        }
+
+    }
+}
