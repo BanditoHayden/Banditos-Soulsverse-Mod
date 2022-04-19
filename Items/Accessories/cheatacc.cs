36dbef1721c8cb2a07acbee9b51f73327f28680d
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-04-18 17:22:43 -0700

    Add files via upload

---
+++ Items/Accessories/cheatacc.cs
@@ -0,0 +1,48 @@
+﻿   
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using SoulsMod.Buffs;
+namespace SoulsMod.Items.Accessories
+{
+
+
+    public class cheatacc : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cheat Accessory");
+            Tooltip.SetDefault("Weapon art cooldown immunity");
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+
+            foreach (TooltipLine line2 in tooltips)
+            {
+                if (line2.mod == "Terraria" && line2.Name == "ItemName")
+                {
+                    line2.overrideColor = new Color(Main.DiscoR = 124, Main.DiscoG = 252, Main.DiscoB = 0);
+                }
+            }
+        }
+        public override void SetDefaults()
+        {
+            Item.accessory = true;
+            Item.value = Item.sellPrice(0, 40, 0, 0);
+            Item.rare = 4;
+        }
+
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.buffImmune[ModContent.BuffType<WeaponArtCooldown>()] = true;
+            player.statLifeMax2 += 1000;
+        }
+
+    }
+}
