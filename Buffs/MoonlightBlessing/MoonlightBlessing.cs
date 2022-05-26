c1a17c204ca08e4cc42abf940df231772dea8e1c
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-25 18:14:23 -0700

    Add files via upload

---
+++ Buffs/MoonlightBlessing/MoonlightBlessing.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Threading.Tasks;
+
+namespace SoulsMod.Buffs.MoonlightBlessing
+{
+    public class MoonlightBlessing : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Moonlight Blessing");
+            Description.SetDefault("Increased Damage");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+        }
+    }
+}
