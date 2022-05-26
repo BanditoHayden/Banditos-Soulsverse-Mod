eb77fd665e7402f25b0495760af17f6aeb90a8b5
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-25 18:19:31 -0700

    Add files via upload

--- Items/Weapons/Melee/Greatswords/StarscourgeGreatsword.cs
+++ Items/Weapons/Melee/Greatswords/StarscourgeGreatsword.cs
@@ -10,6 +10,7 @@ using Terraria;
 using Microsoft.Xna.Framework;
 using SoulsMod.Buffs;
 using System.Collections.Generic;
+using SoulsMod.Projectiles.WeaponArts;
 
 namespace SoulsMod.Items.Weapons.Melee.Greatswords
 {
@@ -23,7 +24,7 @@ namespace SoulsMod.Items.Weapons.Melee.Greatswords
         {
 
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-            Tooltip.SetDefault("Every [c/FFD700:third] attack, spawn a star at your mouse position");
+            Tooltip.SetDefault("Every [c/FFD700:third] attack, spawn a star at your mouse position\nRight Click to use weapon art\n[c/FFD700:Starcaller Cry]\nPulls enemies towards you");
         }
         public override void SetDefaults()
         {
@@ -36,14 +37,13 @@ namespace SoulsMod.Items.Weapons.Melee.Greatswords
             Item.noUseGraphic = true;
             // Use Properties
             Item.useStyle = ItemUseStyleID.Shoot;
-            Item.useAnimation = 30;
-            Item.useTime = 30;
+            Item.useAnimation = 50;
+            Item.useTime = 50;
             Item.UseSound = SoundID.Item1;
             Item.autoReuse = false;
             // Weapon Properties
-            Item.damage = 32;
+            Item.damage = 40;
             Item.knockBack = 6.6f;
-            Item.crit = 10;
             Item.DamageType = DamageClass.Melee;
             // Projectile Properties
             Item.shoot = ModContent.ProjectileType<StarscourgeGreatswordHeld>();
@@ -55,6 +55,40 @@ namespace SoulsMod.Items.Weapons.Melee.Greatswords
             return true;
         }
         int shoot;
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                Item.shoot = ModContent.ProjectileType<BlackHole>();
+                Item.shootSpeed = 0.1f;
+                player.immune = true;
+                player.immuneTime = 60;
+                if (player.HasBuff(ModContent.BuffType<WeaponArtCooldown>()))
+                {
+                    player.immune = false;
+                    return false;
+                }
+            }
+            else
+            {
+                Item.shoot = ModContent.ProjectileType<StarscourgeGreatswordHeld>();
+                Item.shootSpeed = 12f;
+            }
+            return base.CanUseItem(player);
+        }
+
+        public override void UseStyle(Player player, Rectangle heldItemFrame)
+        {
+            if (player.whoAmI == Main.myPlayer)
+            {
+                if (player.altFunctionUse == 2 && !player.HasBuff<WeaponArtCooldown>())
+                {
+                    player.AddBuff(Item.buffType, 3600, true);
+                    
+                    player.AddBuff(ModContent.BuffType<WeaponArtCooldown>(), 3600, true);
+                }
+            }
+        }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
             Vector2 target = Main.screenPosition + new Vector2(Main.mouseX, Main.mouseY);
@@ -64,9 +98,14 @@ namespace SoulsMod.Items.Weapons.Melee.Greatswords
                 ceilingLimit = player.Center.Y - 200f;
             }
             shoot++;
-
+            if (player.altFunctionUse == 2)
+            {
+                shoot = 0;
+                Projectile.NewProjectile(source, position,velocity, ModContent.ProjectileType<BlackHole>(), 0, knockback);
+            }
             if (shoot == 3)
             {
+               
                 shoot = 0;
                 for (int i = 0; i < 1; i++)
                 {
@@ -99,26 +138,7 @@ namespace SoulsMod.Items.Weapons.Melee.Greatswords
             return false;
         }
         
-
-
-
-
-
-
-
-
-
-
-
-
-        public override void AddRecipes()
-        {
-            CreateRecipe()
-           .AddIngredient(ItemID.MeteoriteBar, 25)
-            .AddIngredient(ItemID.FallenStar, 10)
-            .AddTile(TileID.Anvils)
-            .Register();
-        }
+       
 
     }
 }
