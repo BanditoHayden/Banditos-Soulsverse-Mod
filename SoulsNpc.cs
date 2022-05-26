8e235bc9f7d3257c8c1efd7f3c49de8369d130e8
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-05-25 18:20:53 -0700

    Add files via upload

---
+++ SoulsNpc.cs
@@ -0,0 +1,51 @@
+﻿using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria;
+using Terraria.ID;
+using SoulsMod.Items.Weapons.Melee.Reapers;
+using Terraria.GameContent.ItemDropRules;
+using SoulsMod.Items.Weapons.Melee.Greatswords;
+using SoulsMod.Items.Weapons.Melee.Spears;
+
+namespace SoulsMod
+{
+    public class SoulsNpc : GlobalNPC
+    {
+        public override void SetupShop(int type, Chest shop, ref int nextSlot)
+        {
+           if (Main.hardMode)
+            {
+                if (type == NPCID.Merchant)
+                {
+                    shop.item[nextSlot].SetDefaults(ModContent.ItemType<CrescentSickle>());
+                    nextSlot++;
+                }
+            }
+            if (NPC.downedBoss3) // Skele
+            {
+                if (type == NPCID.ArmsDealer)
+                {
+                    shop.item[nextSlot].SetDefaults(ModContent.ItemType<RifleSpear>());
+                    nextSlot++;
+                }
+            } 
+        }
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (npc.type == NPCID.MeteorHead)
+            {
+                npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<StarscourgeGreatsword>(), 75));
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ SoulsPlayer.cs
@@ -0,0 +1,27 @@
+﻿using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using SoulsMod.Buffs.MoonlightBlessing;
+using Terraria;
+
+namespace SoulsMod
+{
+    public class SoulsPlayer : ModPlayer
+    {
+        
+        public override void ModifyWeaponDamage(Item item, ref StatModifier damage)
+        {
+            if (Player.HasBuff(ModContent.BuffType<MoonlightBlessing>()))
+            {
+                damage += 0.5f;
+            }
+        }
+        public override void Kill(double damage, int hitDirection, bool pvp, PlayerDeathReason damageSource)
+        {
+            if (ModContent.GetInstance<Config>().SoundToggle)
+            {
+                SoundEngine.PlaySound(SoundLoader.GetLegacySoundSlot(Mod, "Sounds/Custom/DeathSound"));
+            }
+        }
+    }
+}
